948014878c5458072484f26e2d430e2448824ece
tamir <tamir.nahum@mail.huji.ac.il>
2018-05-27 21:52:20 +0300

    commit

---
+++ app/src/main/java/com/tamir/postpc2018networking/MyImagesAdapter.java
@@ -0,0 +1,39 @@
+package com.tamir.postpc2018networking;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MyImagesAdapter extends RecyclerView.Adapter<MyImagesAdapter.ViewHolder> {
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+        }
+
+        public void bindViewHolder() {
+        }
+    }
+}
